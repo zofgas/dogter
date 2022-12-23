f90032b99a940e5e6a64ab3cbe274cd296fe2e77
zofgas <zosiagasienica@wp.pl>
2022-12-23 19:19:01 +0100

    Strona główna

--- tweets/views.py
+++ tweets/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
+from .models import Tweet
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "pages/home.html", context={}, status=200)
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    return HttpResponse(f"<h1>Hello {tweet_id}</h1>")
+    """
+    REST API VIEW
+    """
+    data = {
+        "id": tweet_id,
+        #"image_path": obj.image.url,
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not found"
+        status = 404
+    return JsonResponse(data, status=status)
