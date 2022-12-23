68e8c1b96af97a7913458012e40c96fee08c72a5
zofgas <zosiagasienica@wp.pl>
2022-12-23 18:52:05 +0100

    Hello World

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+def home_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Hello World</h1>")
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    return HttpResponse(f"<h1>Hello {tweet_id}</h1>")
