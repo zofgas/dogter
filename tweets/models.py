cb09765305f4755c6ac0866893014738ba5b0c33
zofgas <zosiagasienica@wp.pl>
2022-12-23 18:34:35 +0100

    Stworzenie modelu

--- tweets/models.py
+++ tweets/models.py
@@ -1,4 +1,5 @@
 from django.db import models
 
 class Tweet(models.Model):
-    content = models.TextField()
+    content = models.TextField(blank=True, null=True)
+    image =  models.FileField(upload_to='images/', blank=True, null=True)
