306a66f48122ec0be98f1f81ca9f66bf63acf749
zofgas <zosiagasienica@wp.pl>
2022-12-24 12:44:20 +0100

    Stworzenie listy

--- tweets/views.py
+++ tweets/views.py
@@ -6,6 +6,18 @@ from .models import Tweet
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
+def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW
+    """
+    qs = Tweet.objects.all()
+    tweet_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {
+        "isUser": False,
+        "response": tweet_list
+    }
+    return JsonResponse(data)
+
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     """
     REST API VIEW
