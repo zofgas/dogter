dd2663fc524a3b6da5e693db06737a967da6a370
Jasiek34 <j.krajewski835@gmail.com>
2022-12-27 01:32:59 +0100

    create_tweet

--- tweets/views.py
+++ tweets/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404, JsonResponse
+from .forms import TweetForm
 from .models import Tweet
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
+
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    next_url = request.POST.get("next") or None
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        if request.is_ajax():
+            return JsonResponse({}, status=201)
+        if next_url != None:
+            return redirect(next_url)
+        form = TweetForm()
+    return render(request, 'components/form.html', context={"form": form})
+
 
 def tweet_list_view(request, *args, **kwargs):
     """
