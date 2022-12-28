9b9905e4220e94b0113475223184d5fac0bf6e11
zofgas <zosiagasienica@wp.pl>
2022-12-28 15:22:39 +0100

    Created React App

---
+++ dogter-web/src/App.js
@@ -0,0 +1,36 @@
+import React, {useEffect, useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [tweets, setTweets] = useState([])
+  useEffect(() => {
+    const tweetItems = [{"content": "It works!!"}, {"content": "Hi dog"}]
+    setTweets(tweetItems)
+  }, [])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {tweets.map((tweet, index) => {
+            return <li>{tweet.content}</li>
+          })}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
