b6430182e7940e0b3985a13f4c18002d1a58c4e3
Aleksandra Hinc <olka.hinc@gmail.com>
2022-12-30 06:44:13 +0100

    Tweet list view by username - frontend

--- dogter-web/src/tweets/components.js
+++ dogter-web/src/tweets/components.js
@@ -7,7 +7,7 @@ import {
 export function TweetsComponent(props) {
   const textAreaRef = React.createRef()
   const [newTweets, setNewTweets] = useState([])
-
+  const canTweet = props.canTweet === "false" ? false : true
   const handleBackendUpdate = (response, status) => {
     let tempNewTweets = [...newTweets]
     if(status === 201) {
@@ -27,15 +27,15 @@ export function TweetsComponent(props) {
     textAreaRef.current.value = ''
   } 
   return <div className={props.className}>
-    <div className='col-12 mb-3'>
+    {canTweet === true && <div className='col-12 mb-3'>
       <form onSubmit={handleSubmit}>
         <textarea ref={textAreaRef} required={true} className='form-control' name='tweet'>
 
         </textarea>
         <button type='submit' className='btn btn-primary my-3'>Bark</button>
       </form>
-    </div>
-    <TweetList newTweets={newTweets} />
+    </div>}
+    <TweetList newTweets={newTweets} {...props}/>
   </div>
 }
 
@@ -59,9 +59,9 @@ export function TweetList(props) {
             alert("There was an error")
           }
         }
-        apiTweetList(handleTweetListLookup)
+        apiTweetList(props.username, handleTweetListLookup)
     }
-    }, [tweetsInit,tweetsDidSet,setTweetsDidSet])
+    }, [tweetsInit, tweetsDidSet, setTweetsDidSet, props.username])
 
     const handleDidRetweet = (newTweet) => {
       const updateTweetsInit = [...tweetsInit]
