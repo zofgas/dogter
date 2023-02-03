57921a99647275cf9b867c3fa0d1c107cd65f8d8
awar1 <s205826@sggw.edu.pl>
2023-02-03 21:17:00 +0100

    Updated Js

--- dogter/settings.py
+++ dogter/settings.py
@@ -153,9 +153,9 @@ if DEBUG:
     DEFAULT_RENDERER_CLASSES += [
         'rest_framework.renderers.BrowsableAPIRenderer',
     ]
-    DEFAULT_AUTHENTICATION_CLASSES+=[
-        'dogter.rest_api.dev.DevAuthentication'
-    ]
+    # DEFAULT_AUTHENTICATION_CLASSES+=[
+    #     'dogter.rest_api.dev.DevAuthentication'
+    # ]
 REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': DEFAULT_AUTHENTICATION_CLASSES,
